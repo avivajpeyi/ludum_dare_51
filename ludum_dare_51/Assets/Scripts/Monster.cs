dcbf1e8400f6bd1441cf094e1f944f18ad6839dc
Alishan Premani <alishan96@gmail.com>
2022-10-03 00:57:26 -0400

    boolean addition to monster script

--- ludum_dare_51/Assets/Scripts/Monster.cs
+++ ludum_dare_51/Assets/Scripts/Monster.cs
@@ -8,19 +8,31 @@ public class Monster : MonoBehaviour
 
     float fireRate;
     float nextFire;
+    bool monsterAlive;
 
     // Start is called before the first frame update
     void Start()
     {
+        monsterAlive = true;
         fireRate = 4f;
         nextFire = Time.time;
+    }
+
+    public void turnOnMonster()
+    {
+        monsterAlive = true;
+    }
+
+    public void turnOffMonster()
+    {
+        monsterAlive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         // check if time to fire
-        if (Time.time > nextFire)
+        if (monsterAlive && Time.time > nextFire)
         {
             Instantiate(laser, transform.position, Quaternion.identity);
             nextFire = Time.time + fireRate;
